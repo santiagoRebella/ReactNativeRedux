5a083e894d13eb690d1e5c4800d42c859f648cae
Santiago Rebella <santiago_rebella@hotmail.com>
2019-10-21 02:55:33 -0300

    fixes details

--- src/screens/Details.js
+++ src/screens/Details.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {View, Text} from 'react-native';
 
-export const DetailsScreen = () => {
+export default () => {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Details Screen</Text>
